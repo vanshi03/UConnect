1e4548a015503d95c96fc0da8400e07a7ad60993
vanshi03 <v11mittal@gmail.com>
2023-02-14 18:52:26 +0530

    Database Connection

--- app/src/main/java/com/example/uconnect/Login_Register.java
+++ app/src/main/java/com/example/uconnect/Login_Register.java
@@ -6,30 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import org.w3c.dom.Text;
 public class Login_Register extends AppCompatActivity {
-    Button login,signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_register);
-        login = findViewById(R.id.button2);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
-                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
-            }
-        });
-        signup = findViewById(R.id.button);
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Signup.class);
-                startActivity(intent);
-                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
-            }
-        });
+
+//                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
     }
 }

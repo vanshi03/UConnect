338327b5853ba601aafc7be4e119fba577ff070e
vanshi03 <v11mittal@gmail.com>
2023-02-15 16:29:59 +0530

    OTP Authentication

--- app/src/main/java/com/example/uconnect/Login_Register.java
+++ app/src/main/java/com/example/uconnect/Login_Register.java
@@ -1,7 +1,6 @@
 package com.example.uconnect;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,9 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
-import org.w3c.dom.Text;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import java.util.concurrent.TimeUnit;
+
+@SuppressWarnings("ALL")
 public class Login_Register extends AppCompatActivity {
 
     EditText enternumber;
@@ -25,16 +29,42 @@ public class Login_Register extends AppCompatActivity {
 
         enternumber = findViewById(R.id.input_mobile_number);
         getotpbutton = findViewById(R.id.buttongetotp);
-
+        ProgressBar progressBar = findViewById(R.id.progressbar_sending_otp);
         getotpbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!enternumber.getText().toString().trim().isEmpty()){
                     if ((enternumber.getText().toString().trim()).length()==10){
-                        Intent intent = new Intent(getApplicationContext(), OtpVerification.class);
-                        intent.putExtra( "mobile", enternumber.getText().toString());
-                        startActivity(intent);
+                        progressBar.setVisibility(View.VISIBLE);
+                        getotpbutton.setVisibility(View.INVISIBLE);
 
+                        PhoneAuthProvider.getInstance().verifyPhoneNumber("+91" + enternumber.getText().toString(),
+                                60, TimeUnit.SECONDS, Login_Register.this,
+                                new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                                    @Override
+                                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                        progressBar.setVisibility(View.GONE);
+                                        getotpbutton.setVisibility(View.VISIBLE);
+                                    }
+
+                                    @Override
+                                   public void onVerificationFailed(@NonNull FirebaseException e) {
+                                        progressBar.setVisibility(View.GONE);
+                                        getotpbutton.setVisibility(View.VISIBLE);
+                                       Toast.makeText(Login_Register.this, "Please check internet connection", Toast.LENGTH_SHORT).show();
+                                    }
+
+                                    @Override
+                                    public void onCodeSent(@NonNull String backendotp, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                        progressBar.setVisibility(View.GONE);
+                                        getotpbutton.setVisibility(View.VISIBLE);
+                                        Intent intent = new Intent(getApplicationContext(), OtpVerification.class);
+                                        intent.putExtra( "mobile", enternumber.getText().toString());
+                                        intent.putExtra("backendotp",backendotp);
+                                        startActivity(intent);
+                                    }
+                                }
+                        );
                     }
                     else {
                         Toast.makeText(Login_Register.this, "Please enter correct number", Toast.LENGTH_SHORT).show();

4650bed73235663c4aa52f831b372e82005c1a91
vanshi03 <v11mittal@gmail.com>
2023-02-23 14:42:38 +0530

    Changes(23/2/23)

--- app/src/main/java/com/example/uconnect/Login.java
+++ app/src/main/java/com/example/uconnect/Login.java
@@ -14,7 +14,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 public class Login extends AppCompatActivity {
     EditText mobile_number;
     protected void onCreate(Bundle savedInstanceState) {

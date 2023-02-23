da02a63c8c5e8fab09536d36a8d213bd3cf6fd8b
vanshi03 <v11mittal@gmail.com>
2023-02-23 19:19:34 +0530

    Location Manually

--- app/src/main/java/com/example/uconnect/Location_Activity.java
+++ app/src/main/java/com/example/uconnect/Location_Activity.java
@@ -2,7 +2,9 @@ package com.example.uconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Location_Activity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Location_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        Button manual = findViewById(R.id.LocationManually);
+        manual.setOnClickListener(v -> {
+            Intent i = new Intent(Location_Activity.this,MapsActivity.class);
+            startActivity(i);
+        });
     }
 }
